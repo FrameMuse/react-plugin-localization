3f0d2ce9f79d0809654b6f14eb55cc8e4138442d
Valery Zinchenko <minicablestone@gmail.com>
2021-12-12 08:35:16 +0300

    made selector required

--- hook.ts
+++ hook.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 
 import Localization, { LocalizationJSON, Localize } from "./controller"
 
-function useLocalization<Selected extends object = LocalizationJSON>(selector?: (ll: LocalizationJSON) => Selected): Partial<Selected> {
+function useLocalization<Selected extends object = LocalizationJSON>(selector: (ll: LocalizationJSON) => Selected): Partial<Selected> {
   const [localization, updateLocalization] = useState(Localize(selector))
   useEffect(() => {
     return Localization.onTransition(() => updateLocalization(Localize(selector)))
